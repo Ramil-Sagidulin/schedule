b219b68c024b0b23e0c7df66966ff61b9e4ebf35
Ramil Sagidulin <ramilsagidulinr@gmail.com>
2025-01-08 00:20:40 +0500

    добавил возможность переносить событие по месяцам вперед

--- src/ calendar/calendar.js
+++ src/ calendar/calendar.js
@@ -1,12 +1,12 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import '../ calendar/calendar.css';
 import Modal from "../modal/modal";
+
 
 function Calendar() {
     let [currentDate, setCurrentDate] = useState(new Date());
     let currentYear = currentDate.getFullYear();
     let currentMonth = currentDate.getMonth();
-    let [currentDay, setCurrentDay] = useState(currentDate.getDate());
     let [modalWindowState, setModalWindowState] = useState(false);
     let [eventDetailsModalState, setEventDetailsModalState] = useState(false);
     let dayOfTheWeek = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
@@ -17,23 +17,31 @@ function Calendar() {
     let lastDayOfMonth = new Date(currentYear, currentMonth, daysInMonth).getDay();
     let [eventInformation, setEventInformation] = useState({});
     let [eventKey, setEventKey] = useState();
-    let toDate = new Date()
+    let toDate = new Date();
     let toYear = toDate.getFullYear();
     let toMonth = toDate.getMonth();
     let toDay = toDate.getDate();
     firstDayOfMonth = firstDayOfMonth === 0 ? 6 : firstDayOfMonth - 1;
     lastDayOfMonth = lastDayOfMonth === 0 ? 6 : lastDayOfMonth - 1;
     let daysArray = [];
+    let [eInfo, setEInfo] = useState('');
+
     for (let i = 0; i < firstDayOfMonth; i++) {
         daysArray.push(<div className='empty' key={`empty-start-${i}`}></div>);
     }
     for (let i = 1; i <= daysInMonth; i++) {
         let dayKey = `${i}-${currentMonth}-${currentYear}`;
+        let isPastDate = new Date(currentYear, currentMonth, i).getTime() <= toDate.getTime();
         daysArray.push(
-            <div draggable={true} onDragStart={()=>dragStart(dayKey)}
-                 onDragOver={(event) => (event.preventDefault())}
-                 onDrop={()=>dragDrop(i, dayKey)} className={dayKey === `${toDay}-${toMonth}-${toYear}` ? 'day currentDay' : (dayKey in eventInformation ? 'day eventDay' : 'day')}
-                 onClick={() => event(i)} key={i}>
+            <div
+                draggable={!isPastDate}
+                onDragStart={() => !isPastDate && dragStart(dayKey)}
+                onDragOver={(event) => event.preventDefault()}
+                onDrop={() => !isPastDate && dragDrop(dayKey)}
+                className={dayKey === `${toDay}-${toMonth}-${toYear}` ? 'day currentDay' : (dayKey in eventInformation ? 'day eventDay' : 'day')}
+                onClick={() => event(i)}
+                key={i}
+            >
                 {i}
             </div>
         );
@@ -41,19 +49,22 @@ function Calendar() {
     for (let i = lastDayOfMonth + 1; i < 7; i++) {
         daysArray.push(<div className='empty' key={`empty-end-${i}`}></div>);
     }
-    let [eInfo,setEInfo]=useState('')
+
     function dragStart(day) {
-            setEInfo(eventInformation[day])
-            delete eventInformation[day]
+        setEInfo({ key: day, info: eventInformation[day] });
     }
 
-
-    function dragDrop(i, day){
-        if(eInfo && new Date(currentYear, currentMonth, i).getTime() > toDate.getTime()){
-            console.log('--- drag end')
-            setEventInformation((prev)=>({...prev,[day]:eInfo}))
+    function dragDrop(day) {
+        if (eInfo) {
+            setEventInformation((prev) => ({ ...prev, [day]: eInfo.info }));
+            setEventInformation((prev) => {
+                const newEventInformation = { ...prev };
+                delete newEventInformation[eInfo.key];
+                return newEventInformation;
+            });
         }
     }
+
     function event(day) {
         let key = `${day}-${currentMonth}-${currentYear}`;
         setEventKey(key);
@@ -64,27 +75,39 @@ function Calendar() {
                 setModalWindowState(true);
             }
         }
-        setModalInputValue('')
+        setModalInputValue('');
     }
 
     function addNewEvent() {
-        setEventInformation((prevState) => ({...prevState, [eventKey]: modalInputValue}));
+        setEventInformation((prevState) => ({ ...prevState, [eventKey]: modalInputValue }));
         setModalWindowState(false);
     }
 
     function changeMonth(month) {
-        setCurrentDate(new Date(currentYear, currentMonth + month, 1))
+        setCurrentDate(new Date(currentYear, currentMonth + month, 1));
     }
+
+    let timeoutId;
+    function buttonDragOver(event) {
+        event.preventDefault();
+        if (!timeoutId) {
+            timeoutId = setTimeout(() => {
+                changeMonth(1);
+            }, 2000);
+        }
+    }
+
+    function buttonDragLeave() {
+        clearTimeout(timeoutId);
+    }
+
     return (
         <div className='calendar'>
             <Modal open={modalWindowState} close={() => setModalWindowState(false)}>
                 <div>
                     <div className='modal__title'>Добавить новое событие</div>
-                    <input className='modal__input'
-                           onChange={(event) => setModalInputValue(event.target.value)}></input>
-                    <button disabled={modalInputValue === '' ? true : false} className='modal__button'
-                            onClick={() => addNewEvent()}>Добавить
-                    </button>
+                    <input className='modal__input' onChange={(event) => setModalInputValue(event.target.value)}></input>
+                    <button disabled={modalInputValue === ''} className='modal__button' onClick={() => addNewEvent()}>Добавить</button>
                 </div>
             </Modal>
 
@@ -97,7 +120,7 @@ function Calendar() {
 
             <div className='buttonBox'>
                 <button className='button' onClick={() => changeMonth(-1)}>назад</button>
-                <button className='button' onClick={() => changeMonth(1)}>вперед</button>
+                <button className='button' onClick={() => changeMonth(1)} onDragOver={buttonDragOver} onDragLeave={buttonDragLeave}>вперед</button>
             </div>
             <div className='month'>{monthName[currentMonth]} {currentYear}</div>
             <div className='weekdays'>
